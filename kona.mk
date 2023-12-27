c3906c63917c946e63d0f2dd76d44a50b88bfeef
HDzungx <118291317+hdzungx@users.noreply.github.com>
2023-12-27 10:00:34 +0000

    munch:  Add ViPER4Android FX support
    * Requires:
    git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX

--- kona.mk
+++ kona.mk
@@ -13,6 +13,9 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 30 31 32 33
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+#Viper
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
